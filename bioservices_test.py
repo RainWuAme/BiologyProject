3fabdc0dde0960340b3b867198b189d0235008b7
Nick Chen <fasionout@gmail.com>
2018-09-18 21:11:47 +0800

    update the test code.

--- bioservices_test.py
+++ bioservices_test.py
@@ -1,4 +1,4 @@
 from bioservices import UniProt
 u = UniProt(verbose=False)
-data = u.search("tax1bp1", frmt="tab", limit=6,columns="entry name,length,id, genes")
+data = u.search("YER030W or YPL190C or YGR086C", frmt="tab", limit=20,columns="entry name,length,id, genes,comment(FUNCTION)")
 print(data) 
