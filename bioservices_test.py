90c8a91905d9a04fe630f37bd0c572631e12277b
unknown <prokonp@gmail.com>
2018-10-08 20:57:12 +0800

    commit to push

--- bioservices_test.py
+++ bioservices_test.py
@@ -48,24 +48,32 @@ data_list.to_csv("test_rain.csv")
 # word "Saccharomyces cerevisiae" to get the result we want.
 # =============================================================================
 #%% Pramod180927 UniProt data
+
 from bioservices import UniProt
 import pandas as pd
+import re
+import numpy as np
 
+u = UniProt(verbose=False)
 index = pd.ExcelFile('individual hits analysis_yeast_1.xlsx')
 dfs = pd.read_excel(index, sheet_name=1)
 #print(dfs)
 ele = dfs['Histatin-5_unique']
-data = []
-for i in ele[0:]:
-    Result = u.search(i+" Saccharomyces cerevisiae", frmt="tab", limit=1,\
-                    columns="entry name,length,\
-                id, genes, organism")
-#    data = data.extend[]
-Result.to_csv('out.csv')
-    
-#    print(data)
 
+for i in ele[0:19]:
+    data = u.search(i+" Saccharomyces cerevisiae", frmt="tab", limit=1,\
+                    columns="entry name,length, id, genes, organism")
+#   print(data)
 
+#data.to_csv("test_rain",columns=['entry name','length','id','genes','organism'])
+# Change the data to list
+#test = data.split('\n')
+test = re.split('\n|\t',data)
+del test[-1]
+test_resh = np.reshape(test,[2,5])
+
+data_list = pd.DataFrame(test_resh,columns=['entry name','length','id','genes','organism'])
+data_list.to_csv("test_rain.csv")
 
 
 
