9c92623bcc8996a8860b26f83216e1478e1e6962
Nick Chen <fasionout@gmail.com>
2018-10-08 21:09:10 +0800

    Get all data from Rain's code and put in the CSV file.

--- bioservices_test.py
+++ bioservices_test.py
@@ -10,6 +10,43 @@ data = u.search("YER030W", frmt="tab", limit=20,columns="entry name,length,\
                 id, genes, organism-id")
 print(data)
 
+#%% Nick181008 Get all data from Rain's code
+
+from bioservices import UniProt
+import pandas as pd
+import re
+import numpy as np
+
+u = UniProt(verbose=False)
+index = pd.ExcelFile('individual hits analysis_yeast_1.xlsx')
+dfs = pd.read_excel(index, sheet_name=1)
+print(dfs)
+ele = dfs['Histatin-5_unique']
+
+total_data = []
+
+for i in ele[0:]:
+    data = u.search(i+" Saccharomyces cerevisiae", frmt="tab", limit=1,\
+                    columns="entry name,length, id, genes, organism")
+    test = re.split('\n|\t',data)
+    del test[-1]
+    total_data.append(test)
+    
+    print(data)
+
+a = len(total_data)
+data_list1 = [] 
+count = [5,6,7,8,9]
+
+for i in range(0,a):
+    for j in count:
+        data_list1.append(total_data[i][j])
+    
+data_list2 = np.reshape(data_list1,[a,5])
+data_list3 = pd.DataFrame(data_list2, columns=['entry name','length','id','genes','organism'])
+data_list3.to_csv("test_nick.csv")
+
+
 # =============================================================================
 # In here we can use package "bioservices" to extract the information from 
 # Unipro database. Most of the information that we want can be get.
